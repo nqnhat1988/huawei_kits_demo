6f4865bd08b26949ce9432cd5cf743d3da472a4d
nhatdear <nhatdear1988@gmail.com>
2020-08-06 11:25:20 +0700

    refactoring for generic package

---
+++ app/src/h_plus_g/java/com/nhat/huaweikit/demo/huawei/injection/module/ApplicationComponent.kt
@@ -0,0 +1,36 @@
+package com.nhat.huaweikit.demo.huawei.injection.module
+
+import android.app.Application
+import com.nhat.huaweikit.demo.huawei.BaseApp
+import com.nhat.huaweikit.demo.huawei.injection.module.*
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.support.AndroidSupportInjectionModule
+import javax.inject.Singleton
+
+@Singleton
+@Component(
+    modules = [
+        ApplicationModule::class,
+        AndroidSupportInjectionModule::class,
+        DataModule::class,
+//        CacheModule::class,
+        DomainModule::class,
+        PresentationModule::class,
+//        RemoteModule::class,
+        MobileServicesModule::class, //TODO remove when complete refactoring
+        com.example.mobile_services.module.MobileServicesModule::class,
+        UiModule::class
+    ]
+)
+interface ApplicationComponent {
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun application(application: Application): Builder
+
+        fun build(): ApplicationComponent
+    }
+
+    fun inject(app: BaseApp)
+}
